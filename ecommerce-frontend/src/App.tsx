44e66ed809ec00479a4e59cb661c3de11574aec4
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2025-12-22 02:42:42 +0500

    shipping ui is completed

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
 const Cart = lazy(() => import("./pages/Cart"));
+const   Shipping = lazy(() => import("./pages/Shipping"));
 
 // Admin Routes Importing
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -37,6 +38,11 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+
+          {/* LoggedIn Routes  */}
+          <Route>
+            <Route path="/shipping" element={<Shipping />} />
+          </Route>
 
 
           {/* Admin Routes  */}
