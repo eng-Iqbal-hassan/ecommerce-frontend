ffc45be885c22225414ba50ebdfc0d173d0ca06f
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2025-12-22 15:16:59 +0500

    checkout page styles are added

--- ecommerce-frontend/src/pages/Shipping.tsx
+++ ecommerce-frontend/src/pages/Shipping.tsx
@@ -1,5 +1,6 @@
 import { useState, type ChangeEvent } from "react"
 import { BiArrowBack } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
 const Shipping = () => {
 
@@ -15,10 +16,12 @@ const changeHandler = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => 
     setShippingInfo(prev=>({...prev, [e.target.name] : e.target.value}))
 };
 
+const navigate = useNavigate();
+
   return (
 
     <div className="shipping">
-      <button><BiArrowBack /></button>
+      <button className="back-btn" onClick={()=> navigate("/cart")}><BiArrowBack /></button>
       <form>
         <h1>Shipping Address</h1>
         <input 
@@ -59,7 +62,7 @@ const changeHandler = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => 
             type="number" 
             name="pinCode" 
             placeholder="pin code" 
-            value={shippingInfo.state} 
+            value={shippingInfo.pinCode} 
             onChange={changeHandler} 
         />
         <button type="submit">Pay Now</button>
