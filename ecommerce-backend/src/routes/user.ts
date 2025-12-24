c28625ce8b7d7926ac647935bad0f7bdac340bf7
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2025-12-24 23:05:01 +0500

    some of the user routes are authenticated

--- ecommerce-backend/src/routes/user.ts
+++ ecommerce-backend/src/routes/user.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
 
 const app = express.Router();
 
@@ -7,12 +8,12 @@ const app = express.Router();
 app.post("/new", newUser);
 
 // route: /api/v1/user/all
-app.get("/all", getAllUsers)
+app.get("/all", adminOnly, getAllUsers)
 
 // route: /api/v1/user/dynamicId
 // app.get("/:id", getUser);
 // app.delete("/:id",deleteUser);
 // AS both routes are same so we can chain them by the syntax
-app.route("/:id").get(getUser).delete(deleteUser)
+app.route("/:id").get(getUser).delete(adminOnly, deleteUser)
 
 export default app;
